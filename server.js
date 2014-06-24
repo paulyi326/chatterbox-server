c8e3ba49d8142a7745ed60d2af4578d5a5d9a6ea
paulyi326 <paul.yi326@gmail.com>
2014-06-24 10:35:15 -0700

    moved basic server to root and renamed it server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /* Import node's http module: */
 var http = require("http");
 var url = require('url');
-var handleRequest = require('./request-handler.js').handleRequest;
+var handleRequest = require('./server/request-handler.js').handleRequest;
 
 /* Every server needs to listen on a port with a unique number. The
  * standard port for HTTP servers is port 80, but that port is
