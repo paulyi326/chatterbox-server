72e1d79611963552bd8361211485216116c63eb5
paulyi326 <paul.yi326@gmail.com>
2014-06-23 17:00:32 -0700

    able to read user POST data

--- server/request-handler.js
+++ server/request-handler.js
@@ -4,42 +4,86 @@
  * You'll have to figure out a way to export this function from
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
+var url = require('url');
+var storage = {};
 
-var handleRequest = function(request, response) {
-  /* the 'request' argument comes from nodes http module. It includes info about the
-  request - such as what URL the browser is requesting. */
+module.exports = {
+  handleRequest: function(request, response) {
+    console.log(request);
 
-  /* Documentation for both request and response can be found at
-   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
+    if(request.method === "OPTIONS") {
+      // handle options request
+      console.log('heeeeeey');
+      var headers = module.exports.defaultCorsHeaders;
+      headers['Content-Type'] = "text/plain";
+      response.writeHead(200, headers);
+      response.end();
+    } else if (request.method === 'GET') {
+      console.log('GOT IT');
+      console.log(request.url);
+      console.log(url.parse(request.url, true));
+      var headers = module.exports.defaultCorsHeaders;
+      headers['Content-Type'] = "text/plain";
+      response.writeHead(200, headers);
+      response.end();
+    } else if (request.method === 'POST') {
+      var body = "";
+      request.on('data', function (chunk) {
+        body += chunk;
+      });
+      request.on('end', function () {
+        console.log('POSTed: ' + body);
+        response.writeHead(200);
+        response.end();
+      });
+      console.log(body);
+    }
+    // else if (request.method === 'GET') {
+    //   if (path === '/1/messages') {
 
-  console.log("Serving request type " + request.method + " for url " + request.url);
+    //   } else if (path === 'something else') {
 
-  var statusCode = 200;
+    //   }
+    // }
 
-  /* Without this line, this server wouldn't work. See the note
-   * below about CORS. */
-  var headers = defaultCorsHeaders;
 
-  headers['Content-Type'] = "text/plain";
 
-  /* .writeHead() tells our server what HTTP status code to send back */
-  response.writeHead(statusCode, headers);
+    // console.log(response);
+    /* the 'request' argument comes from nodes http module. It includes info about the
+    request - such as what URL the browser is requesting. */
 
-  /* Make sure to always call response.end() - Node will not send
-   * anything back to the client until you do. The string you pass to
-   * response.end() will be the body of the response - i.e. what shows
-   * up in the browser.*/
-  response.end("Hello, World!");
-};
+    /* Documentation for both request and response can be found at
+     * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
 
-/* These headers will allow Cross-Origin Resource Sharing (CORS).
- * This CRUCIAL code allows this server to talk to websites that
- * are on different domains. (Your chat client is running from a url
- * like file://your/chat/client/index.html, which is considered a
- * different domain.) */
-var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
+    console.log("Serving request type " + request.method + " for url " + request.url);
+
+    var statusCode = 200;
+
+    /* Without this line, this server wouldn't work. See the note
+     * below about CORS. */
+    var headers = module.exports.defaultCorsHeaders;
+
+    headers['Content-Type'] = "text/plain";
+
+    /* .writeHead() tells our server what HTTP status code to send back */
+    response.writeHead(statusCode, headers);
+
+    /* Make sure to always call response.end() - Node will not send
+     * anything back to the client until you do. The string you pass to
+     * response.end() will be the body of the response - i.e. what shows
+     * up in the browser.*/
+    response.end("Hello, World!");
+  },
+
+  /* These headers will allow Cross-Origin Resource Sharing (CORS).
+   * This CRUCIAL code allows this server to talk to websites that
+   * are on different domains. (Your chat client is running from a url
+   * like file://your/chat/client/index.html, which is considered a
+   * different domain.) */
+  defaultCorsHeaders: {
+    "access-control-allow-origin": "*",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-headers": "content-type, accept",
+    "access-control-max-age": 10 // Seconds.
+  }
 };
